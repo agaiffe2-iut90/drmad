8430f7161af61e883d24aac3bdcd7f0760430f4d
agaiffe2-iut90 <anna.gaiffe@edu.univ-fcomte.fr>
2025-01-06 14:58:00 +0100

    hello

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Bienvenue  hello dans la boutique</h1>
+        <h1>Bienvenuoiergoiehe  hello dans la boutique</h1>
         <router-view></router-view>
     </div>
 </template>
